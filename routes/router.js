1b891e770552d611d82d1d25761e2675551b6c58
Jordan200315 <jordanmalibiran406@gmail.com>
2024-09-23 20:02:48 +0800

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -6,5 +6,9 @@ chester.get('/home', dan.home);
 chester.get('/about', dan.about);
 chester.get('/contact', dan.contact);
 chester.get('/services', dan.services);
+chester.get('/gallery', dan.gallery);
+chester.get('/_about', dan._about);
+chester.get('/banner', dan.banner);
+chester.get('/_services', dan._services);
 
 module.exports = chester;
